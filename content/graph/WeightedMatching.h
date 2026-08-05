58ebc818c83297751fc81ed162f0120d216943b6
Joshua Andersson <matistjati@outlook.com>
2026-08-05 19:08:38 +0200

    Golf weightedmatching

--- content/graph/WeightedMatching.h
+++ content/graph/WeightedMatching.h
@@ -17,24 +17,24 @@
 template<class T>
 pair<T, vi> weightedMatching(vector<vector<T>> &C) {
 	int i = sz(C), m = i ? sz(C[0]) : 0, c, s, r;
-	vector<T> dist(m), potential(m);
-	vi rowMatch(i), colMatch(m, -1), cols(m), prev(m);
+	vector<T> dist(m), pot(m);
+	vi match(i), rev(m, -1), cols(m), prev(m);
 	T d = 0, nd, cost = 0;
 	while (i--) {
 		rep(c,0,m) dist[c] = C[i][c], cols[c] = c, prev[c] = i;
 		for (s = 0;;) {
 			rep(j,s,m) {
-				c = cols[j], nd = dist[c] - potential[c];
+				c = cols[j], nd = dist[c] - pot[c];
 				if (j == s || d > nd) d = nd, swap(cols[s], cols[j]);
 			}
-			if ((r = colMatch[c = cols[s++]]) == -1) break;
+			if ((r = rev[c = cols[s++]]) == -1) break;
 			rep(j,0,m) if (dist[j] > (nd = C[r][j]-C[r][c]+dist[c]))
 				dist[j] = nd, prev[j] = r;
 		}
 		cost += dist[c];
-		while (s--) potential[cols[s]] = dist[cols[s]] - d;
-		for (; r != i; swap(c, rowMatch[r]))
-			r = colMatch[c] = prev[c];
+		while (s--) pot[cols[s]] = dist[cols[s]] - d;
+		for (; r != i; swap(c, match[r]))
+			r = rev[c] = prev[c];
 	}
-	return {cost, rowMatch};
+	return {cost, match};
 }
